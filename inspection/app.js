4fe363d3e922d3e2a347fb7ec135c13d3c176cfb
srinu <webdeveloper.srinu9@gmail.com>
2024-10-02 13:26:00 +0530

    add react sibling

--- inspection/app.js
+++ inspection/app.js
@@ -1,13 +1,25 @@
-const h1 = React.createElement(
-  "h1",
-  { id: "h1", xyz: "srinu" },
-  "react element"
-);
+/* <div id="main">
+    <div id="sub-2">
+    <h1>hello srinu</h1>
+    </div>
+    <div id="sub-2">
+    <h2>srinu</h2>
+    </div>
+</div> */
+
+const h1 = React.createElement("div", { id: "main" }, [
+  React.createElement(
+    "div",
+    { id: "sub-1",key:"t" },
+    React.createElement("h1", {}, "hello srinu")
+  ),
+  React.createElement(
+    "div",
+    { id: "sub-2" ,key:"y"},
+    React.createElement("h1", {}, "srinu")
+  ),
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(h1);
 
-const heading = document.createElement("h1");
-heading.innerText = "hello";
-console.log(heading);
-
-console.log(h1);
+console.log(h1)
